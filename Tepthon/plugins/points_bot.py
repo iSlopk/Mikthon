c31a0f63a210ba291dba2c7297c100e339f98072
iSlopk <110035853+iSlopk@users.noreply.github.com>
2025-06-15 17:08:49 +0300

    update Tepthon/plugins/points_bot.py

--- Tepthon/plugins/points_bot.py
+++ Tepthon/plugins/points_bot.py
@@ -229,7 +229,7 @@ async def enable_team_mode(event):
 async def disable_team_mode(event):
     """تعطيل وضع الفرق والعودة إلى الوضع الأساسي"""
     if not event.is_group:
-    return await safe_edit_or_reply(event, "❗️يعمل فقط في المجموعات.")
+        return await safe_edit_or_reply(event, "❗️يعمل فقط في المجموعات.")
 TEAM_MODE_STATUS[event.chat_id] = False
 return await safe_edit_or_reply(event, "❌ تم تعطيل وضع الفرق والعودة إلى الوضع الأساسي: عرض نقاط الأفراد.")
 
