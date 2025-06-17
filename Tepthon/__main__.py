42c940487c4b3c1d2aaadb388cf714383ebbd70c
iSlopk <110035853+iSlopk@users.noreply.github.com>
2025-06-17 11:16:42 +0300

    update Tepthon/__main__.py

--- Tepthon/__main__.py
+++ Tepthon/__main__.py
@@ -1,14 +1,21 @@
 import sys, asyncio
 import Tepthon
+"""
 from Tepthon import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
+"""
 from telethon import functions
 from .Config import Config
 from .core.logger import logging
 from .core.session import zedub
 from .utils import mybot, autoname, autovars, saves
+#default
+"""
 from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup
+"""
+#copy
+from .utils import load_plugins, setup_bot
 
-LOGS = logging.getLogger("ســـورس مـيكثــون")
+"""LOGS = logging.getLogger("ســـورس مـيكثــون")"""
 cmdhr = Config.COMMAND_HAND_LER
 
 try:
@@ -53,7 +60,7 @@ try:
 except Exception as e:
     LOGS.error(f"- {e}")
 
-
+"""
 async def startup_process():
     async def MarkAsViewed(channel_id):
         from telethon.tl.functions.channels import ReadMessageContentsRequest
@@ -84,9 +91,11 @@ async def startup_process():
 
     
     await verifyLoggerGroup()
+    """
     await load_plugins("plugins")
     await load_plugins("assistant")
     LOGS.info(f"⌔ تـم تنصيـب مـيكثــون بنجـاح ✓ \n⌔ لـ إظهـار الأوامــر أرسـل (.الاوامر)")
+    """
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
     if PM_LOGGER_GROUP_ID != -100:
@@ -94,7 +103,7 @@ async def startup_process():
     await startupmessage()
     Catcheck.sucess = True
     return
-
+   """
 
 zedub.loop.run_until_complete(startup_process())
 
