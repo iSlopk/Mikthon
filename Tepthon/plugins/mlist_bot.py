c272576ec8b96cb9b343564733e3ce3afbd79a95
iSlopk <110035853+iSlopk@users.noreply.github.com>
2025-06-22 18:39:32 +0300

    update Tepthon/plugins/mlist_bot.py

--- Tepthon/plugins/mlist_bot.py
+++ Tepthon/plugins/mlist_bot.py
@@ -55,7 +55,7 @@ async def mlist_handler(event):
         MLIST_DATA[key] = set()
     chat_id, reply_to = key
     names = await get_names(event.client, list(MLIST_DATA[key]))
-    text = "**قـائـمـة الـمـشـرفـيـن الـحـضـور:**\n" + ("\n".join(names) if names else "_لا يوجد أحد بعد_")
+    text = "**قـائـمـة الـمـشـرفـيـن الـحـضـور:**\n" + ("\n".join(names) if names else "ليس هناك مشرف موجود 👀")
     btns = [
         [
             Button.inline("Log In 🟢", data=f"mlogin|{chat_id}|{reply_to}"),
