c8ec748d485a9be7c400532e7ac998158d7f4422
iSlopk <110035853+iSlopk@users.noreply.github.com>
2025-06-22 19:20:40 +0300

    update Tepthon/plugins/mlist_bot.py

--- Tepthon/plugins/mlist_bot.py
+++ Tepthon/plugins/mlist_bot.py
@@ -77,10 +77,6 @@ async def mlist_in(event):
     asyncio.create_task(delete_later(msg))
     user = await event.client.get_entity(user_id)
 
-    else:
-        msg = await event.reply("أنت لست ضمن القائمة!")
-        asyncio.create_task(delete_later(msg))
-
 @zedub.bot_cmd(pattern="^/out$")
 async def mlist_out(event):
     key = get_key(event)
@@ -118,9 +114,6 @@ async def mlogin_handler(event):
     await event.answer("تم تسجيل حضورك ✅", alert=False)
     user = await event.client.get_entity(user_id)
 
-    else:
-        msg = await event.reply("أنت لست ضمن القائمة!")
-        asyncio.create_task(delete_later(msg))
 
 @zedub.tgbot.on(events.CallbackQuery(pattern=r"mlogout\|(-?\d+)\|(\d+)"))
 async def mlogout_handler(event):
